fb205cb567e939418e921ee8c0903bf15730c29b
Moises <martosmoises60@gmail.com>
2018-11-20 18:15:07 -0500

    Manejando la pantalla

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
+var pantalla = document.getElementById('display')
 var teclas = document.querySelectorAll('.tecla');
 teclas.forEach(function(e){//forEach es una propiedad que recorre todos los elementos del array
 	e.addEventListener("click", function(){
 		var entrada = this.id;
 		if (entrada != "") {
 			console.log(entrada);
+      procesador(entrada);
 		}
 	})
 })
+
+function procesador(entrada){
+  var valor = pantalla.textContent;
+
+  if (entrada === "on") {
+    pantalla.textContent = "0";
+  } else {
+    pantalla.textContent = valor + entrada;
+  }
+
+}
